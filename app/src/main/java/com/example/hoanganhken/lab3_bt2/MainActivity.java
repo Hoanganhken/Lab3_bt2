41fb41172cef7e0e8d9d4142ddc83c553c02de14
Hoanganhken <hoanganhken98@gmail.com>
2017-09-24 22:21:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/hoanganhken/lab3_bt2/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.hoanganhken.lab3_bt2;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MainActivity extends AppCompatActivity {
+    TextView txtStatus;
+    LoginButton login_button;
+    CallbackManager callbackManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtStatus= (TextView) findViewById(R.id.txtStatus);
+        login_button= (LoginButton) findViewById(R.id.login_button);
+        callbackManager=CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                txtStatus.setText("Login SuCess\n" + loginResult . getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                txtStatus.setText("Login Cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+                txtStatus.setText("Login Error: "+ error.getMessage());
+            }
+        });
+
+        // Add code to print out the key hash
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.hoanganhken.lab3_bt2",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
